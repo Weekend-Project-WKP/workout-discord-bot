845d01e468b1f5b28e6a609852d1818bc0c0ba99
Greg Cherry <Gcherry2@gmail.com>
2024-12-23 09:12:22 -0500

    add initial mongo db connection

---
+++ internal/services/mongoDbService.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+  
+  func MongoDBInit() {
+	mongoDbPassword := os.Getenv("MONGODB_PASSWORD")
+	if mongoDbPassword == "" {
+		log.Fatal("MONGODB_PASSWORD not set in environment")
+	}
+
+	mongoDbUserId := os.Getenv("MONGODB_USERID")
+	if mongoDbUserId == "" {
+		log.Fatal("MONGODB_USERID not set in environment")
+	}
+	
+	connectionString := fmt.Sprintf("mongodb+srv://%v:%v@workoutbot.ee1do.mongodb.net/?retryWrites=true&w=majority&appName=workoutbot", mongoDbUserId, mongoDbPassword) 
+	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(connectionString).SetServerAPIOptions(serverAPI)
+  
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+	  panic(err)
+	}
+  
+	defer func() {
+	  if err = client.Disconnect(context.TODO()); err != nil {
+		panic(err)
+	  }
+	}()
+  
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+	  panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+  }
+  
