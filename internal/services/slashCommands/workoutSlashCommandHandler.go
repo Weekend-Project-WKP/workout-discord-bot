4cc84b6c472693f2202a18e8780da6cf30b07650
gcherry <gcherry2@gmail.com>
2025-01-19 14:45:11 -0500

    description update

--- internal/services/slashCommands/workoutSlashCommandHandler.go
+++ internal/services/slashCommands/workoutSlashCommandHandler.go
@@ -9,7 +9,7 @@ import (
 func WorkoutSlashCommandHandler(s *discordgo.Session, i *discordgo.InteractionCreate){
 	//name := i.ApplicationCommandData().Options[0].StringValue()
 
-		// Respond with a greeting
+		// Respond with a summary of the points added
 		err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 			Type: discordgo.InteractionResponseChannelMessageWithSource,
 			Data: &discordgo.InteractionResponseData{
